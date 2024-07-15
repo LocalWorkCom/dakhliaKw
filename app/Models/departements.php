2e68f4535d2f2c85fd6df561feff64c2d9612e0a
hHanan13 <hanannino13@gmail.com>
2024-07-15 16:46:34 +0300

    departments apis crud

--- app/Models/departements.php
+++ app/Models/departements.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class departements extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'manager_director',
+        'ass_manager_director',
+    ];
 }
