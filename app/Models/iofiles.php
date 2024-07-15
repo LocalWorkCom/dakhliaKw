92a091c5b19c1a96bd7b7c05aa6eda5d3b54cb91
Rasha Shalan <rasha.shalan@brmaja.com>
2024-07-15 10:38:10 +0300

    db migrations

---
+++ app/Models/iofiles.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class iofiles extends Model
+{
+    use HasFactory;
+}
