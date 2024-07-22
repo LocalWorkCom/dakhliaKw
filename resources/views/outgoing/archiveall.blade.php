c809a2b45747b93413bf63eecda25cffa26ab66c
Omaima9 <omaimasalem933@gmail.com>
2024-07-22 18:02:32 +0300

    toq21

--- resources/views/outgoing/archiveall.blade.php
+++ resources/views/outgoing/archiveall.blade.php
@@ -2,33 +2,35 @@
 
 
 @section('content')
-    <div class="container">
-      
+ 
+<div class="row col-11" dir="rtl">
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item "><a href="#">الرئيسيه</a></li>
+        <li class="breadcrumb-item"><a href="{{ route('Export.index') }}">الصادرات </a></li>
+        <li class="breadcrumb-item active" aria-current="page"> <a href="">   ارشيف الصادر</a></li>
+      </ol>
+    </nav>
+  </div>
 <div class="row ">
     <div class="container welcome col-11">
         <p> ارشيف الصادر</p>
     </div>
 </div>
 
-    <div class="container  col-11 mt-3 p-0 ">
-        <div class="row justify-content-end">
-            <div class="col-auto">
-                <button class="btn-all mt-3">
-                    <a href="{{ route('Export.index') }}" style="color:#0D992C;">رجوع<img
-                            src="{{ asset('frontend/images/add-btn')}}" alt=""></a>
-                </button>
-            </div>
-        </div>
-    </div>
-       
-    
-
+    <div class="container  col-11 mt-4 p-0 ">
+      
         <div class="row ">
             <div class="mb-3">
                 {{-- <input type="text" id="global_search" class="form-control" placeholder="بحث ..."> --}}
             </div>
             {!! $dataTable->table(['class' => 'table table-responsive table-bordered table-hover dataTable']) !!}
         </div>
+    </div>
+       
+    
+
+        
    
 
 @endsection
