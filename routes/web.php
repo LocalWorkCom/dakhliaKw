11fd44456830a060d2df64a86971aac7c33c4e8d
Rasha Shalan <rasha.shalan@brmaja.com>
2024-07-23 07:57:00 +0300

    Home statics

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\PostmanController;
 use App\Http\Controllers\settingController;
+use App\Http\Controllers\HomeController;
 
 
 /*
@@ -44,10 +45,8 @@ Route::get('/login', function () {
 
 //  Auth verfication_code
 Route::middleware(['auth'])->group(function () {
-    
-    Route::get('/', function () {
-        return view('welcome');
-    })->name('home');
+         
+    Route::get('/',[HomeController::class,'index'])->name('home');
     
     // Route::any('/user', [UserController::class, 'index'])->name('user.index');
     Route::get('/users/{id}', [UserController::class, 'index'])->name('user.index');
