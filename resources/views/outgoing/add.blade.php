71590512320222b014e973ce3723aa7c6ab5b287
shrookallam <shrookallam67@gmail.com>
2024-07-24 10:29:56 +0300

    finish6

--- resources/views/outgoing/add.blade.php
+++ resources/views/outgoing/add.blade.php
@@ -1,7 +1,6 @@
 @extends('layout.main')
 
 @push('style')
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
 @endpush
 @section('title')
     أضافه
