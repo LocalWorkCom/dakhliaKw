06c7bddcdaf2b447192b09232a1fe64944c86552
Omaima9 <omaimasalem933@gmail.com>
2024-07-30 09:50:18 +0300

    toqa

--- resources/views/grads/index.blade.php
+++ resources/views/grads/index.blade.php
@@ -98,7 +98,7 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"> &times;
                     </button>
                 </div>
-                <div class="modal-body  d-flex justify-content-center mt-5 mb-5">
+                <div class="modal-body  mt-5 mb-5">
                     <form class="edit-grade-form" id="edit-form" action=" {{ route('grads.update') }}" method="POST">
                         @csrf
                         <div class="form-row mx-md-2 ">
