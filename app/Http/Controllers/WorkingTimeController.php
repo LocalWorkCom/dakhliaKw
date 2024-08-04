241c5564ac01f08afc331cf2a40cf6ddd944057b
Dalia Adel Ali <34524724+DaliaAdel@users.noreply.github.com>
2024-08-04 10:59:20 +0300

    working time table

---
+++ app/Http/Controllers/WorkingTimeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\WorkingTime;
+use App\Http\Requests\StoreWorkingTimeRequest;
+use App\Http\Requests\UpdateWorkingTimeRequest;
+
+class WorkingTimeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreWorkingTimeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(WorkingTime $workingTime)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(WorkingTime $workingTime)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateWorkingTimeRequest $request, WorkingTime $workingTime)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(WorkingTime $workingTime)
+    {
+        //
+    }
+}
