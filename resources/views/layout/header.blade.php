b1d31275ddbba35e0899437130b4788e474baddd
Dalia Adel Ali <34524724+DaliaAdel@users.noreply.github.com>
2024-08-04 13:15:52 +0300

    working time  create - show - view -edit 2

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -163,6 +163,13 @@
                                                 <a href="{{ route('grads.index') }}">الرتب العسكرية</a>
                                             </li>
                                         @endif
+
+                                        <li>
+                                            <img src="{{ asset('frontend/images/governorates.svg') }}" alt="logo"
+                                                style="margin-left: 7px;">
+                                            <a href="{{ route('working_time.index') }}">فترات العمل </a>
+                                        </li>
+
                                         @if (Auth::user()->hasPermission('view job'))
                                             <li>
                                                 <img src="{{ asset('frontend/images/jobs.svg') }}" alt="logo"
