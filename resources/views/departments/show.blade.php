6b2a4cb8f5043ed8119e2e6c397d8e1cb8053d86
hHanan13 <34395039+hHanan13@users.noreply.github.com>
2024-08-06 18:24:48 +0300

    hanan

--- resources/views/departments/show.blade.php
+++ resources/views/departments/show.blade.php
@@ -6,14 +6,14 @@
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item "><a href="{{ route('home') }}">الرئيسيه</a></li>
-            <li class="breadcrumb-item"><a href="{{ route('departments.index') }}">القطاعات </a></li>
+            <li class="breadcrumb-item"><a href="{{ route('departments.index') }}">الادارات </a></li>
             <li class="breadcrumb-item active" aria-current="page"> <a href="#">تفاصيل الادارة</a></li>
         </ol>
     </nav>
 </div>
 <div class="row ">
     <div class="container welcome col-11">
-        <p> القطاعات </p>
+        <p> الادارات </p>
     </div>
 </div>
 <section style="direction: rtl;">
