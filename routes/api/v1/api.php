2f771de85957e096416734db9dcc160a49d16cf4
Dalia Adel Ali <34524724+DaliaAdel@users.noreply.github.com>
2024-08-12 16:50:02 +0300

    add Violation

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -20,5 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::post('/login', 'App\Http\Controllers\Api\UserController@login');
 Route::post('/resetpassword', 'App\Http\Controllers\Api\UserController@reset_password');
-
+Route::post('/check_military_number', 'App\Http\Controllers\Api\UserController@check_military_number');
+Route::any('/Violation_type/{id}', 'App\Http\Controllers\Api\ViolationController@get_Violation_type');
+Route::post('/add_Violation', 'App\Http\Controllers\Api\ViolationController@add_Violation');
 
