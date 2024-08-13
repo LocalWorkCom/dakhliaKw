94c4869c8d9b105d1e03024ae9b1f8577732a828
sara55mohamed <smohamedamer4@gmail.com>
2024-08-13 13:01:11 +0300

    table34

--- resources/views/inspectorMission/index.blade.php
+++ resources/views/inspectorMission/index.blade.php
@@ -3,21 +3,22 @@
     مــهام التــفتــيش
 @endsection
 
-
 @section('content')
     <section>
         <div class="row ">
             <div class="container welcome col-11">
                 <div class="d-flex justify-content-between">
+                    <!-- Title Section -->
                     <p> مــهام التــفتــيش </p>
 
+                    <!-- Print Button and Legend -->
                     <div class="d-flex ">
-                        <button class="btn-all px-3 " style="color: #FFFFFF; background-color: #274373;"
-                            onclick="window.print()">
+                        <button class="btn-all px-3 " style="color: #FFFFFF; background-color: #274373;" onclick="printDiv()">
                             <img src="{{ asset('frontend/images/print.svg') }}" alt=""> طباعة
                         </button>
+                        
                         <div class="colors  d-flex mx-5">
-
+                            <!-- Legend for different types of shifts -->
                             <div class="night rounded p-1 px-3 mx-1"> ليل</div>
                             <div class="morning  rounded p-1 px-3 mx-1 ">صبح</div>
                             <div class="afternoon rounded p-1 px-3 mx-1">عصر</div>
@@ -32,7 +33,7 @@
         </div>
         <br>
 
-
+        <!-- Main Table Section -->
         <div class="row col-12" id="days-table">
             <div class="container col-12">
                 <div class="col-12" dir="rtl">
@@ -41,6 +42,7 @@
                         @foreach ($Groups as $Group)
                             <thead>
                                 <tr>
+                                    <!-- Table Headers for Group -->
                                     <th scope="col" rowspan="2" style="background-color: #a5d0ffbd;">العدد</th>
                                     <th scope="col" rowspan="2" style="background-color: #e4f1ffbd;">
                                         {{ $Group->name }}</th>
@@ -49,29 +51,37 @@
                                     @endforeach
                                 </tr>
                                 <tr>
+                                    <!-- Day Numbers for the Group -->
                                     @foreach ($Group['days_num'] as $num)
                                         <th scope="col" class="{{ $num }}">{{ $num }}</th>
                                     @endforeach
                                 </tr>
                             </thead>
                             <tbody>
+                                <!-- Teams and Their Missions -->
                                 @foreach ($Group['teams'] as $team)
                                     <tr class="group-table">
+                                        <!-- Team Name Row -->
                                         <td colspan="2"
                                             style=" text-align: center;color: white;background-color: darkgreen;">
                                             {{ $team->name }}</td>
                                         @foreach ($Group['days_num'] as $index => $num)
+                                            <!-- Colors for Each Day in the Team -->
                                             <td style="background-color:{{ $team['colors'][$index] }}"></td>
                                         @endforeach
                                     </tr>
 
+                                    <!-- Inspectors and Their Missions -->
                                     @foreach ($team['inspectors'] as $index => $inspector)
                                         <tr>
+                                            <!-- Inspector Number -->
                                             <td style="background-color: #a5d0ffbd">{{ $count }}</td>
+                                            <!-- Inspector Name -->
                                             <td style="color: white;background-color: darkcyan;">
                                                 {{ $inspector->name }}</td>
                                             @foreach ($inspector['missions'] as $index2 => $mission)
                                                 @if ($mission)
+                                                    <!-- Determine Class Based on Mission Type -->
                                                     @php
                                                         $class = '';
                                                         if ($mission->day_off) {
@@ -81,9 +91,11 @@
                                                         }
                                                     @endphp
 
+                                                    <!-- Mission Details -->
                                                     <td class="{{ $class }}"
                                                         style="background-color: {{ $class != '' ? '' : $inspector['colors'][$index2] }}">
                                                         <ul>
+                                                            <!-- Mission Points -->
                                                             @if (!$mission->day_off && isset($mission['points']) && count($mission['points']) > 0)
                                                                 @foreach ($mission['points'] as $point)
                                                                     <li style="color: white;font-weight: bold">
@@ -91,6 +103,7 @@
                                                                 @endforeach
                                                             @endif
 
+                                                            <!-- Instant Missions -->
                                                             @if (!$mission->day_off && isset($mission['instant_missions']) && count($mission['instant_missions']) > 0)
                                                                 @foreach ($mission['instant_missions'] as $instant_mission)
                                                                     <li class="urgent">{{ $instant_mission->label }}</li>
@@ -99,6 +112,7 @@
                                                         </ul>
                                                     </td>
                                                 @else
+                                                    <!-- Default Background if No Mission -->
                                                     <td style="background-color: #756262"></td>
                                                 @endif
                                             @endforeach
@@ -110,9 +124,35 @@
                         @endforeach
 
                     </table>
-
                 </div>
             </div>
         </div>
     </section>
 @endsection
+    <script>
+        function printDiv() {
+            // Select the section you want to print
+            var divToPrint = document.getElementById('days-table'); // Change to the ID of the table section
+
+            // Create a new window for printing
+            var newWin = window.open('', 'Print-Window');
+
+            newWin.document.open();
+
+            // Write the HTML content to the new window and include the table's content
+            newWin.document.write('<html><head><title>Print</title>');
+            // newWin.document.write(
+            //     '<link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/your-css-file.css') }}">'
+            //     ); // Include any required CSS
+            newWin.document.write('</head><body onload="window.print()">');
+            newWin.document.write(divToPrint.innerHTML);
+            newWin.document.write('</body></html>');
+
+            newWin.document.close();
+
+            // Close the print window after printing
+            setTimeout(function() {
+                newWin.close();
+            }, 10);
+        }
+    </script>
