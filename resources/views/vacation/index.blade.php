00ca84295dd4a28f3f6feb0f326aea632c3f4934
sara55mohamed <smohamedamer4@gmail.com>
2024-08-14 11:27:57 +0300

    table view5

--- resources/views/vacation/index.blade.php
+++ resources/views/vacation/index.blade.php
@@ -9,25 +9,24 @@
 
     <div class="row ">
         <div class="container welcome col-11">
-        <div class="d-flex justify-content-between">
-            <p> الاجــــــازات </p>
-     
-        @if (Auth::user()->hasPermission('create EmployeeVacation'))
-                        <button type="button" class="wide-btn"
-                            onclick="window.location.href='{{ route('vacation.add', $id) }}'">
-                           
-                            اضافة جديد  <img src="{{ asset('frontend/images/add-btn.svg') }}" alt="img">
-                        </button>
-                    @endif
+            <div class="d-flex justify-content-between">
+                <p> الاجــــــازات </p>
+
+                @if (Auth::user()->hasPermission('create EmployeeVacation'))
+                    <button type="button" class="btn-all-2 mt-1 px-3 mx-3" style="color: #274373;" onclick="window.location.href='{{ route('vacation.add', $id) }}'">
+
+                        اضافة جديد <img src="{{ asset('frontend/images/time.svg') }}" alt="img">
+                    </button>
+                @endif
+            </div>
+        </div>
     </div>
-    </div> 
-    </div> 
     <br>
     <div class="row">
         <div class="container  col-11 mt-3 p-0 ">
             <div class="row " dir="rtl">
                 <div class="form-group mt-4  mx-2 col-12 d-flex ">
-                  
+
 
                 </div>
             </div>
@@ -103,33 +102,45 @@
                             ],
                             columnDefs: [{
                                 targets: -1,
+                               
                                 render: function(data, type, row) {
                                     var showVacation = "<?php echo Auth::user()->hasPermission('view EmployeeVacation'); ?>";
-                                    // Using route generation correctly in JavaScript
                                     var showUrl = '{{ route('vacation.show', ':id') }}';
                                     var acceptUrl = '{{ route('vacation.accept', ':id') }}';
                                     var rejectUrl = '{{ route('vacation.reject', ':id') }}';
 
                                     showUrl = showUrl.replace(':id', row.id);
+                                    acceptUrl = acceptUrl.replace(':id', row.id);
+                                    rejectUrl = rejectUrl.replace(':id', row.id);
+
                                     var showButton = '';
                                     var acceptButton = '';
                                     var rejectButton = '';
 
                                     if (showVacation) {
                                         showButton =
-                                            `<a href="${showUrl}" class="edit btn  btn-sm" style="background-color: #375a97;"><i class="fa fa-eye"></i> عرض</a>`;
-                                    }
-                                    if (row.VacationStatus == 'مقدمة') {
-                                        acceptButton =
-                                            `<a href="${acceptUrl}" class="edit btn  btn-sm" style="background-color: #375a97;"><i class="fa fa-eye"></i> موافقة</a>`;
-                                        rejectButton =
-                                            `<a href="${rejectUrl}" class="edit btn  btn-sm" style="background-color: #375a97;"><i class="fa fa-eye"></i> رفض</a>`;
+                                            `<a href="${showUrl}" class="edit btn btn-sm" style="background-color: #375a97;"><i class="fa fa-eye"></i> عرض</a>`;
                                     }
 
-                                    // Checking if the vacation start date condition is met
+                                    if (row.VacationStatus == 'مقدمة') {
+                                        acceptButton = `
+                                            <form id="acceptForm" action="${acceptUrl}" method="POST" style="display:inline;">
+                                                @csrf
+                                                <a href="#" class="edit btn btn-sm" style="background-color: #28a745;" onclick="document.getElementById('acceptForm').submit();">
+                                                    <i class="fa fa-check"></i> موافقة
+                                                </a>
+                                            </form>`;
+
+                                        rejectButton = `
+                                            <form id="rejectForm" action="${rejectUrl}" method="POST" style="display:inline;">
+                                                @csrf
+                                                <a href="#" class="edit btn btn-sm" style="background-color: #dc3545;" onclick="document.getElementById('rejectForm').submit();">
+                                                    <i class="fa fa-times"></i> رفض
+                                                </a>
+                                            </form>`;
+                                    }
 
                                     return `${showButton}${acceptButton}${rejectButton}`;
-
                                 }
 
                             }],
