3db4a217db89e65d906fca8cca4e6a1ed610e93e
salsabil3322 <szzz123t0@gmail.com>
2024-08-25 15:34:39 +0300

    fix

--- database/migrations/2024_08_25_064854_add_is_holiday_working_tree_times.php
+++ database/migrations/2024_08_25_064854_add_is_holiday_working_tree_times.php
@@ -11,18 +11,25 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::table('working_tree_times', function (Blueprint $table) {
+            // Drop the foreign key constraint
+            $table->dropForeign(['working_time_id']);
+            
+            // Modify the column
+            $table->integer('working_time_id')->nullable()->change();
+            
+            // Recreate the foreign key constraint
+            $table->foreign('working_time_id')->references('id')->on('working_times');
+        });
+        
+        // Add 'is_holiday' column if it doesn't exist
         if (!Schema::hasColumn('working_tree_times', 'is_holiday')) {
-
             Schema::table('working_tree_times', function (Blueprint $table) {
                 $table->boolean('is_holiday')->default(0);
             });
         }
-        if (Schema::hasColumn('working_tree_times', 'working_time_id')) {
-            Schema::table('working_tree_times', function (Blueprint $table) {
-                $table->integer('working_time_id')->nullable()->change();
-            });
-        }
     }
+    
 
     /**
      * Reverse the migrations.
