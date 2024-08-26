1f30c97d6b06e1f6aa70ae05875deff8d8f8f2ea
Toqa-Hatem <toqahatem802@gmail.com>
2024-08-26 13:22:43 +0300

    new

--- resources/views/grouppoints/create.blade.php
+++ resources/views/grouppoints/create.blade.php
@@ -6,7 +6,8 @@
     نقاط الوزاره
 @endsection
 @section('content')
-    <div class="row col-11" dir="rtl">
+<div class="row " dir="rtl">
+<div class="container  col-11" style="background-color:transparent;">
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item "><a href="/">الرئيسيه</a></li>
@@ -14,6 +15,7 @@
                 <li class="breadcrumb-item active" aria-current="page"> <a href=""> أنشاء مجموعه للنقاط</a></li>
             </ol>
         </nav>
+</div>
     </div>
  <div class="row ">
         <div class="container welcome col-11">
@@ -27,7 +29,7 @@
         <div class="row" dir="rtl">
             <div id="first-container" class="container moftsh col-11 mt-3 p-0 pb-3">
                 <div class="form-row mx-2 mb-2">
-                    <h3 class="pt-3 px-md-5 px-3">اضف مجموعه</h3>
+                    <h3 class="pt-3 px-md-4 px-3">اضف مجموعه</h3>
                     @if (session('success'))
                     <div class="alert alert-success">
                         {{ session('success') }}
@@ -42,7 +44,7 @@
                         </ul>
                     </div>
                 @endif
-                    <div class="input-group moftsh px-md-5 px-3 pt-3">
+                    <div class="input-group moftsh px-md-4 px-3 pt-3">
                         <label class="pb-3" for="name">ادخل اسم المجموعه</label>
                         <input type="text" id="name" name="name" class="form-control" placeholder="قطاع واحد"
                             required />
