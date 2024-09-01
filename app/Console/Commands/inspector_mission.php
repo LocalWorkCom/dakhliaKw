89cf3482a0d836f87918e71b0fbabc112f916f4f
sara55mohamed <smohamedamer4@gmail.com>
2024-09-01 10:25:07 +0300

    issue3

--- app/Console/Commands/inspector_mission.php
+++ app/Console/Commands/inspector_mission.php
@@ -61,6 +61,7 @@ class inspector_mission extends Command
                 $total_days_in_cycle = $WorkingTree->working_days_num + $WorkingTree->holiday_days_num;
                 // for loob by num of day's monthly
                 $day_of_month_val = $GroupTeam->last_day;
+
                 for ($day_of_month = $day_of_month_val; $day_of_month <= $num_days; $day_of_month++) {
                     // check day off or not 
 
@@ -89,7 +90,7 @@ class inspector_mission extends Command
                     }
 
 
-                
+
                     // insert data for monthly
                     $inspectorMission = new InspectorMission();
                     $inspectorMission->inspector_id = $Inspector;
@@ -114,6 +115,81 @@ class inspector_mission extends Command
                     // Move to the next day
                     $date = date('Y-m-d', strtotime($date . ' +1 day'));
                 }
+                // if ($GroupTeam->last_day == 3) {
+
+                //     dd($day_of_month);
+                // }
+                if ($GroupTeam->last_day > 1) {
+                    $firstLoop = $day_of_month - $GroupTeam->last_day;
+                    $day_in_cycle++;
+                    for ($i = $firstLoop; $i < $num_days; $i++) {
+
+
+                        // $is_day_off =  $WorkingTree->is_holiday;
+                        // if not  day off get working tree
+
+                        $WorkingTreeTime =
+                            WorkingTreeTime::where('working_tree_id', $WorkingTree->id)
+                            ->where('day_num', $day_in_cycle)
+                            ->first();
+                        if (!$WorkingTreeTime) {
+                            $day_in_cycle = 1;
+                            $WorkingTreeTime =
+                                WorkingTreeTime::where('working_tree_id', $WorkingTree->id)
+                                ->where('day_num', $day_in_cycle)
+                                ->first();
+                        }
+                     
+
+
+
+                        // if ($day_in_cycle == 4) {
+
+                        // }
+
+
+                        $user_id  = Inspector::find($Inspector)->user_id;
+                        if ($vacation_days == 0) {
+
+                            $EmployeeVacation = EmployeeVacation::where('employee_id', $user_id)->where('start_date', '=',  $date)->first(); //1/9/2024
+                            if ($EmployeeVacation) {
+                                $vacation_days = $EmployeeVacation->days_number; //3
+                            }
+                        }
+
+
+                        // insert data for monthly
+                        $inspectorMission = new InspectorMission();
+                        $inspectorMission->inspector_id = $Inspector;
+                        $inspectorMission->group_id = $GroupTeam->group_id;
+                        $inspectorMission->group_team_id = $GroupTeam->id;
+                        $inspectorMission->working_tree_id = $GroupTeam->working_tree_id;
+                        $inspectorMission->working_time_id = $WorkingTreeTime->working_time_id ? $WorkingTreeTime->working_time_id : null;
+                        $inspectorMission->date = $date;
+                        $inspectorMission->day_number = $day_in_cycle;
+                        if ($vacation_days != 0) {
+
+
+                            $inspectorMission->vacation_id = $EmployeeVacation->id;
+                        }
+                        $inspectorMission->day_off =  $WorkingTreeTime->working_time_id ? 0 : 1;
+                        $inspectorMission->save();
+                        if ($vacation_days != 0) {
+
+                            $vacation_days--;
+                        }
+
+                        // Move to the next day
+                        $date = date('Y-m-d', strtotime($date . ' +1 day'));
+                        if ($day_in_cycle > $total_days_in_cycle) {
+
+                            $day_in_cycle = ($i - 1) % $total_days_in_cycle + 1;
+                        } else {
+                            $day_in_cycle++;
+                        }
+                    }
+                }
+
                 if ($ids_inspector_arr[sizeof($ids_inspector_arr) - 1] == $Inspector) {
 
                     $GroupTeam->last_day = $day_in_cycle;
