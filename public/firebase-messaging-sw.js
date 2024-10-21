ae4a25bcb11f7a0214f3e8f31269f25dd100ee18
Rasha Shalan <rasha.shalan@brmaja.com>
2024-10-21 13:10:26 +0300

    test firebase

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,7 @@
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyBJE3YuOw1Jl5qDoC_sqyuiPnq3U0qcAdk",
